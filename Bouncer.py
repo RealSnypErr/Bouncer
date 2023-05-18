10cfca5f7819278f1cefe11149afedf389897940
RealSnypErr <109794413+RealSnypErr@users.noreply.github.com>
2023-05-18 10:23:40 +0800

    New stuff
    you can now add bot ids on the fly using the !whitelist command + minor improvements

--- Bouncer.py
+++ Bouncer.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-token = "TOKEN HERE"
+token = Token here
 whitelist = open("whitelist.txt", "r")
 data = whitelist.read()
 authorized_bots = data.split("\n")
@@ -18,14 +18,31 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    log_channel = client.get_channel("Log channel ID here")
+    log_channel = await client.fetch_channel(LOG CHANNEL ID HERE)
     if member.bot:
         if member.id not in authorized_bots:
+            print(f"Bot {member.id} isnt authorized")
             await member.kick(reason="Bot not authorized")
             msg = (f"**-----------------** \nBot name: {member.name}\nBot ID: {member.id}\n**Check audit log to check who invited the bot**")
-            embed=discord.Embed(title="**UNAUTHORIZED BOT KICKED**", description=(f"Place the bot's ID to whitelist.txt to whitelist this bot"), color=0xff0000)
+            embed=discord.Embed(title="**UNAUTHORIZED BOT KICKED**", description=(f"Contact Deez#5043 to whitelist this bot"), color=0xff0000)
             embed.add_field(name=(msg), value="**-----------------**", inline=False)
-            embed.set_footer(text=f"Bouncer rev.1 created for {Guild.name}")
+            embed.set_footer(text="Bouncer rev.1")
             await log_channel.send(embed=embed)
+
+@client.command()
+@commands.has_role(WHITELISTER ROLE HERE)
+async def add(ctx, id):
+    global authorized_bots
+    await ctx.send(f"Adding bot {id} (<@{id}>) to the whitelist")
+    with open("whitelist.txt", "a") as file:
+        file.write(f"\n{id}")
+
+    authorized_bots = []
+    with open("whitelist.txt", "r") as file:
+        data = file.read().strip()
+        authorized_bots = data.split("\n")
+        authorized_bots = [int(i) for i in authorized_bots]
+
+
             
 client.run(token=token)
